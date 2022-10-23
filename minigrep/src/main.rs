3e4c9d1e288d33283beb0a075ecc6107f395fbe0
unknown <sebaslogo@gmail.com>
2022-10-23 13:46:06 -0500

    added trie

---
+++ minigrep/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| { // unwrap or else unwraps by default otherwise runs code block
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) { // if let destructures run -> Result<Ok, Err> and matches to Err enum
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+
+}
--- stuff/src/main.rs
+++ stuff/src/main.rs
@@ -1,5 +1,11 @@
 use std::fs::File;
+use std::fs;
 
 fn main() {
-    let f : u32 = File::open("Hello.txt");
+    let f = File::open("Hello.txt").expect("Failed to open Hello.txt");
+
+    println!("{:?}", f);
+
+    let s : String = fs::read_to_string("Hello.txt").expect("Failed to read file to string");
+    println!("{}", s);
 }
