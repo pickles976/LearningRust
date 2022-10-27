db8bf577b0eb12794203705ad1cb83ac26455be0
unknown <sebaslogo@gmail.com>
2022-10-27 08:54:33 -0500

    Changed project structure

--- huffman/src/lib.rs
+++ huffman/src/lib.rs
@@ -1,500 +1,65 @@
-use std::cmp::{Ordering, Reverse};
-use std::collections::BinaryHeap;
-use fxhash::FxHashMap;
-use std::option::Option;
+pub mod node;
+pub mod utils;
+
+use std::time::Instant;
 use bytebuffer::ByteBuffer;
+use fxhash::FxHashMap;
+use crate::utils::{count_characters, get_leaves, get_heap, encode_contents, rebuild_tree };
 
+// Encodes a Huffman String to a bytearray
+pub fn encode(contents: &String) -> ByteBuffer {
 
-#[derive(PartialEq, Eq, PartialOrd, Debug)]
-pub struct Node {
-    pub freq: u32,
-    pub c: Option<char>,
-    pub r: Option<Box<Node>>,
-    pub l: Option<Box<Node>>,
-}
+    let now = Instant::now();
 
-impl Ord for Node {
+    // 1. Count characters by frequency
+    let map = count_characters(&contents);
 
-    // TODO: why is this cmp implementation not working?
-    // Using Reverse is ugly and annoying
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.freq.cmp(&other.freq)
-    }
-}
+    // 2. Sort HashMap entries by frequency
+    let leaves = get_leaves(map);
 
-// TODO: possibly create a tree struct that holds the root node and some cleaner interfaces?
-impl Node {
+    // 3. build heap/tree
+    let heap = get_heap(leaves);
 
-    pub fn leaf(c: char, freq: u32) -> Node {
-        Node {                         
-            freq,
-            c: Some(c),
-            l: None,
-            r: None,
-        }
-    }
+    // 3a. get codes
+    let mut codes: FxHashMap<char, String> = FxHashMap::default();
+    heap.peek().unwrap().0.get_codes("".to_string(), &mut codes);
 
-    pub fn node (right: Option<Node>, left: Option<Node>) -> Node {
+    // 3a. get characters
+    let mut characters: String = "".to_string();
+    heap.peek().unwrap().0.get_character_order(&mut characters);
 
-        let right = right.unwrap();
-        let left = left.unwrap();
+    // 4. Encode tree  structureinto binary
+    let mut binary_string = "".to_string();
+    heap.peek().unwrap().0.save_tree(&mut binary_string);
 
-        Node{
-            freq: right.freq + left.freq,
-            c: None,
-            l: Some(Box::new(left)),
-            r: Some(Box::new(right)),
-        }
-    }
+    // 5. Build a buffer
+    let byte_buffer: ByteBuffer = encode_contents(&binary_string, &characters, &contents, codes);
 
-    // get_codes all nodes and save char -> code mappings
-    pub fn get_codes(&self, s: String, map: &mut FxHashMap<char, String>){
+    println!("Compressed {}kb file in {}ms", contents.len() / 1000, now.elapsed().as_millis());
 
-        // go right
-        if let Some(node) = &self.r {
-            let mut temp = s.clone();
-            temp.push('1');
-            node.get_codes(temp, map)
-        }
-
-        // go left
-        if let Some(node) = &self.l {
-            let mut temp = s.clone();
-            temp.push('0');
-            node.get_codes(temp, map)
-        }
-
-        if let Some(c) = self.c {
-            map.insert(c, s);
-        }
-
-    }
-
-    // write tree to string of ones and zeros
-    pub fn save_tree(&self, s: &mut String){
-
-        if self.c.is_some() {
-            s.push('1');
-        } else {
-            s.push('0');
-        }
-
-        // go right
-        if let Some(node) = &self.r {
-            node.save_tree(s);
-        }
-
-        // go left
-        if let Some(node) = &self.l {
-            node.save_tree(s);
-        }
-
-    }
-
-    // populate the tree leaves with characters
-    pub fn populate_tree(mut self, byte_buffer: &mut ByteBuffer) -> Self {
-
-        // go right
-        if let Some(node) = self.r {
-            self.r = Some(Box::new(node.populate_tree(byte_buffer)));
-        }
-
-        // go left
-        if let Some(node) = self.l {
-            self.l = Some(Box::new(node.populate_tree(byte_buffer)));
-        }
-
-        if let Some(_c) = self.c {
-            self.c = char::from_u32(byte_buffer.read_u32());
-        }
-
-        self
-
-    }
-
-    // use bytearray to navigate tree until we reach a leaf
-    pub fn decode_bytearray(&self, output: &mut String, byte_buffer: &mut ByteBuffer) {
-
-        match self.c {
-            Some(c) => {
-                output.push(c);
-            },
-            None => {
-
-                let bit = byte_buffer.read_bit();
-    
-                if bit { // build leaf
-                    if let Some(right) = &self.r {
-                        right.decode_bytearray(output, byte_buffer);
-                    }
-                } else {
-                    if let Some(left) = &self.l {
-                        left.decode_bytearray(output, byte_buffer);
-                    }
-                }
-    
-            },
-        }
-            
-    }
-
-    pub fn get_character_order(&self, characters: &mut String){
-
-        match self.c {
-            Some(c) => characters.push(c),
-            None => {
-                if let Some(right) = &self.r {
-                    right.get_character_order(characters);
-                }
-
-                if let Some(left) = &self.l {
-                    left.get_character_order(characters);
-                }
-            }
-        }
-
-    }
-
-}
-
-pub fn count_characters(contents: &String) -> FxHashMap<char, u32> {
-
-    let mut map: FxHashMap<char, u32> = FxHashMap::default();
-
-    contents.chars().for_each(|c| *map.entry(c).or_default() += 1 );
-
-    map
-
-}
-
-pub fn get_leaves(map: FxHashMap<char, u32>) -> Vec<Node> {
-
-    let mut leaves = Vec::new();
-    
-    for k in map.keys() {
-        leaves.push(Node::leaf(k.clone(), map.get(k).unwrap().clone()));
-    }
-
-    leaves.sort();
-
-    leaves
-}
-
-pub fn get_heap(leaves: Vec<Node>) -> BinaryHeap<Reverse<Node>> {
-
-    let mut heap = BinaryHeap::new();
-
-    for node in leaves {
-        heap.push(Reverse(node));
-    }
-
-    // println!("Heap size: {}", heap.len());
-    // println!("Smallest element is: {:?}", heap.peek().unwrap().0);
-
-    while heap.len() > 1 {
-
-        let right = heap.pop().unwrap().0; // is there a better way to do this than unwrapping???
-        let left = heap.pop().unwrap().0; // and zeroing
-        
-        let new_node = Node::node(Some(right), Some(left));
-
-        heap.push(Reverse(new_node));
-
-    }
-
-    heap
-
-}
-
-pub fn encode_contents(binary_string: &String, characters: &String, contents: &String, codes: FxHashMap<char, String>) -> ByteBuffer {
-
-    let mut byte_buffer : ByteBuffer = ByteBuffer::new();
-    
-    // add size to byte buffer
-    byte_buffer.write_u32(contents.chars().count() as u32);
-
-    // add tree to byte buffer
-    binary_string.chars().for_each(|bit| byte_buffer.write_bit(bit == '1'));
-
-    for _i in 0..3 { byte_buffer.flush_bit() };
-
-    // add chars to byte buffer
-    characters.chars().for_each(|c| byte_buffer.write_u32(c.clone() as u32) );
-
-    // for char in file
-    contents.chars().for_each(|c| {
-        let bits = codes.get(&c).unwrap();
-        bits.chars().for_each(|bit| byte_buffer.write_bit(bit == '1'));
-    });
-
-    for _i in 0..3 { byte_buffer.flush_bit() };
     byte_buffer
 
 }
 
-pub fn rebuild_tree(byte_buffer: &mut ByteBuffer) -> Node {
+// Decodes a bytearray containing the tree and compressed data
+pub fn decode(mut byte_buffer: ByteBuffer) -> String {
 
-    if byte_buffer.read_bit() { // build leaf
-        return Node::leaf(0 as char, 0)
-    }
+    let now = Instant::now();
 
-    return Node::node(Some(rebuild_tree(byte_buffer)), Some(rebuild_tree(byte_buffer)))
-    
-}
+    let len = byte_buffer.read_u32();
 
-#[cfg(test)]
-mod tests {
-    use std::collections::FxHashMap;
+    let mut tree = rebuild_tree(&mut byte_buffer);
+    for _i in 0..3 { byte_buffer.flush_bit() };
 
-    use bytebuffer::ByteBuffer;
+    // 2. Load the characters into the tree
+    tree = tree.populate_tree(&mut byte_buffer);
 
-    use crate::{count_characters, get_leaves, get_heap, Node, encode_contents, rebuild_tree};
+    // 3. Traverse the tree and decode the source file into a string
+    let mut out_string = "".to_string();
 
-    #[test]
-    fn test_counting() {
-        let map = count_characters(&"mmmmaao".to_string());
-        assert_eq!(4, map.get(&'m').unwrap().clone());
-        assert_eq!(2, map.get(&'a').unwrap().clone());
-        assert_eq!(1, map.get(&'o').unwrap().clone());
-        assert!(map.get(&'f').is_none());
-    }
+    for _i in 0..len - 1 {tree.decode_bytearray(&mut out_string, &mut byte_buffer)};
 
-    #[test]
-    fn test_tree() {
+    println!("Decompressed {}kb file in {}ms", byte_buffer.len() / 1000, now.elapsed().as_millis());
 
-        /*
-
-            Test that tree shape is:
-
-                None, 7
-                /     \
-            m, 4    None, 3
-                    /       \
-                  a, 2      o, 1
-        */
-
-        // basic tree
-        let contents = "mmmmaao".to_string();
-        let map = count_characters(&contents);
-        let leaves = get_leaves(map);
-        let heap = get_heap(leaves);
-
-        let root = &heap.peek().unwrap().0;
-
-        assert!(root.c.is_none());
-
-        let left = root.l.as_ref().unwrap();
-        assert_eq!('m' , left.c.unwrap());
-
-        let right = root.r.as_ref().unwrap();
-        assert!(right.c.is_none());
-
-        let right_left = right.l.as_ref().unwrap();
-        assert_eq!('a', right_left.c.unwrap());
-
-        let right_right = right.r.as_ref().unwrap();
-        assert_eq!('o', right_right.c.unwrap());
-
-        /*
-
-                    Test that tree shape is:
-
-                        None, 11
-                        /       \
-                None, 7       None, 4
-                /    \      /       \
-            None, 4   ' '  ?, 2      ?, 2
-            /     \
-          ?, 2    ?, 2
-        */
-
-        // more complex tree
-        let contents = "ll tt oo aa".to_string();
-        let map = count_characters(&contents);
-        let leaves = get_leaves(map);
-        let heap = get_heap(leaves);
-
-        let root = &heap.peek().unwrap().0;
-
-        assert!(root.c.is_none());
-
-        let left = root.l.as_ref().unwrap();
-        assert!(left.c.is_none());
-
-        let right = root.r.as_ref().unwrap();
-        assert!(right.c.is_none());
-
-        let right_left = right.l.as_ref().unwrap();
-        assert!(right_left.c.is_some());
-
-        let right_right = right.r.as_ref().unwrap();
-        assert!(right_right.c.is_some());
-
-        let left_right = left.r.as_ref().unwrap();
-        assert_eq!(' ', left_right.c.unwrap());
-
-        let left_left = left.l.as_ref().unwrap();
-        assert!(left_left.c.is_none());
-
-        let left_left_left = left_left.l.as_ref().unwrap();
-        assert!(left_left_left.c.is_some());
-
-        let left_left_right = left_left.r.as_ref().unwrap();
-        assert!(left_left_right.c.is_some());
-
-    }
-
-    #[test]
-    fn test_tree_representation() {
-
-        let contents = "mmmmaao".to_string();
-        let map = count_characters(&contents);
-        let leaves = get_leaves(map);
-        let heap = get_heap(leaves);
-
-        let mut tree_string = "".to_string();
-
-        let _= &heap.peek().unwrap().0.save_tree(&mut tree_string);
-
-        assert_eq!("00111", tree_string);
-
-        let contents = "ll tt oo aa".to_string();
-        let map = count_characters(&contents);
-        let leaves = get_leaves(map);
-        let heap = get_heap(leaves);
-
-        let mut tree_string = "".to_string();
-
-        let _= &heap.peek().unwrap().0.save_tree(&mut tree_string);
-
-        assert_eq!("001101011", tree_string);
-
-    }
-
-    #[test]
-    fn test_codes() {
-
-        // easy encoding
-        let contents = "mmmmaao".to_string();
-        let map = count_characters(&contents);
-        let leaves = get_leaves(map);
-        let heap = get_heap(leaves);
-
-        let mut codes: FxHashMap<char, String> = FxHashMap::default();
-        heap.peek().unwrap().0.get_codes("".to_string(), &mut codes);
-
-        assert_eq!("0", codes.get(&'m').unwrap());
-        assert_eq!("10", codes.get(&'a').unwrap());
-        assert_eq!("11", codes.get(&'o').unwrap());
-        assert!(codes.get(&'f').is_none());
-
-        // harder encoding
-        let contents = "ll aa oo tt".to_string();
-        let map = count_characters(&contents);
-        let leaves = get_leaves(map);
-        let heap = get_heap(leaves);
-
-        let mut codes: FxHashMap<char, String> = FxHashMap::default();
-        heap.peek().unwrap().0.get_codes("".to_string(), &mut codes);
-
-        assert_eq!("01", codes.get(&' ').unwrap());
-        assert!(codes.get(&'f').is_none());
-
-    }
-
-    #[test]
-    fn test_encoding() {
-
-        // easy tree
-        let contents = "mmmmaao".to_string();
-        let map = count_characters(&contents);
-        let leaves = get_leaves(map);
-        let heap = get_heap(leaves);
-        let mut characters: String = "".to_string();
-        heap.peek().unwrap().0.get_character_order(&mut characters);
-        let mut codes: FxHashMap<char, String> = FxHashMap::new();
-        heap.peek().unwrap().0.get_codes("".to_string(), &mut codes);
-        let mut binary_string = "".to_string();
-        heap.peek().unwrap().0.save_tree(&mut binary_string);
-
-        let mut byte_buffer: ByteBuffer = encode_contents(&binary_string, &characters, &contents, codes);
-
-        let mut byte;
-
-        // 4 (length) + 1 (tree) + 4 * 3 (chars) + 2 (data) = 19 bytes
-        println!("{:?}", byte_buffer.to_bytes());
-        assert_eq!(19, byte_buffer.len());
-
-        // tree 00111 -> 7 LE
-        byte = byte_buffer.read_u32();
-
-        assert_eq!(7, byte);
-        
-        // tree 00111 -> 7 LE
-        let mut byte_8 = byte_buffer.read_u8();
-
-        assert_eq!(56, byte_8);
-
-        // chars 'o', 'a', 'm'
-        byte = byte_buffer.read_u32();
-
-        assert_eq!('o' as u32, byte);
-
-        byte = byte_buffer.read_u32();
-
-        assert_eq!('a' as u32, byte);
-
-        byte = byte_buffer.read_u32();
-
-        assert_eq!('m' as u32, byte);
-
-        // text 0000 1010 11 -> 10, 192
-        byte_8 = byte_buffer.read_u8();
-
-        assert_eq!(10, byte_8);
-
-        byte_8 = byte_buffer.read_u8();
-
-        assert_eq!(192, byte_8);
-
-    }
-
-    #[test]
-    fn test_tree_from_bytes() {
-
-        /*
-
-            Test that tree shape is:
-
-                None, 7
-                /     \
-            m, 4    None, 3
-                    /       \
-                  a, 2      o, 1
-        */
-
-        let bytes = [0, 0, 0, 7, 56, 0, 0, 0, 'o' as u8, 0, 0, 0, 'a' as u8, 0, 0, 0, 'm' as u8];
-
-        let mut byte_buffer = ByteBuffer::new();
-        byte_buffer.write_bytes(&bytes);
-        let size = byte_buffer.read_u32();
-        let mut root = rebuild_tree(&mut byte_buffer);
-        root = root.populate_tree(&mut byte_buffer);
-
-        assert!(root.c.is_none());
-
-        let left = root.l.as_ref().unwrap();
-        assert_eq!('m' , left.c.unwrap());
-
-        let right = root.r.as_ref().unwrap();
-        assert!(right.c.is_none());
-
-        let right_left = right.l.as_ref().unwrap();
-        assert_eq!('a', right_left.c.unwrap());
-
-        let right_right = right.r.as_ref().unwrap();
-        assert_eq!('o', right_right.c.unwrap());
-
-    }
-
+    out_string
 }
--- huffman/src/main.rs
+++ huffman/src/main.rs
@@ -1,66 +1,6 @@
-use fxhash::FxHashMap;
 use std::fs;
 use bytebuffer::ByteBuffer;
-use huffman::{count_characters, get_leaves, get_heap, encode_contents, rebuild_tree};
-use std::time::Instant;
-
-// Encodes a Huffman String to a bytearray
-fn huffman_encode_string(contents: &String) -> ByteBuffer {
-
-    let now = Instant::now();
-
-    // 1. Count characters by frequency
-    let map = count_characters(&contents);
-
-    // 2. Sort HashMap entries by frequency
-    let leaves = get_leaves(map);
-
-    // 3. build heap/tree
-    let heap = get_heap(leaves);
-
-    // 3a. get codes
-    let mut codes: FxHashMap<char, String> = FxHashMap::default();
-    heap.peek().unwrap().0.get_codes("".to_string(), &mut codes);
-
-    // 3a. get characters
-    let mut characters: String = "".to_string();
-    heap.peek().unwrap().0.get_character_order(&mut characters);
-
-    // 4. Encode tree  structureinto binary
-    let mut binary_string = "".to_string();
-    heap.peek().unwrap().0.save_tree(&mut binary_string);
-
-    // 5. Build a buffer
-    let byte_buffer: ByteBuffer = encode_contents(&binary_string, &characters, &contents, codes);
-
-    println!("Compressed {}kb file in {}ms", contents.len() / 1000, now.elapsed().as_millis());
-
-    byte_buffer
-
-}
-
-// Decodes a bytearray containing the tree and compressed data
-fn huffman_decode_bytes(mut byte_buffer: ByteBuffer) -> String {
-
-    let now = Instant::now();
-
-    let len = byte_buffer.read_u32();
-
-    let mut tree = rebuild_tree(&mut byte_buffer);
-    for _i in 0..3 { byte_buffer.flush_bit() };
-
-    // 2. Load the characters into the tree
-    tree = tree.populate_tree(&mut byte_buffer);
-
-    // 3. Traverse the tree and decode the source file into a string
-    let mut out_string = "".to_string();
-
-    for _i in 0..len - 1 {tree.decode_bytearray(&mut out_string, &mut byte_buffer)};
-
-    println!("Decompressed {}kb file in {}ms", byte_buffer.len() / 1000, now.elapsed().as_millis());
-
-    out_string
-}
+use huffman;
 
 fn main() {
 
@@ -68,17 +8,13 @@ fn main() {
     let output = "compressed.txt".to_string();
 
     let contents: String = fs::read_to_string(input).expect("File not found!");
-
-    let byte_buffer: ByteBuffer = huffman_encode_string(&contents);
+    let byte_buffer: ByteBuffer = huffman::encode(&contents);
 
     fs::write(&output, byte_buffer.to_bytes()).expect("Unable to write file");
 
-
     let bytes = fs::read(&output).expect("Failed to open file!");
-
     let byte_buffer = ByteBuffer::from_bytes(&bytes);
-
-    let decoded = huffman_decode_bytes(byte_buffer);
+    let decoded = huffman::decode(byte_buffer);
 
     fs::write("decompressed.txt", decoded).expect("Unable to write data");
 
