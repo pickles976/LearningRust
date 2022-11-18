f6866fb7ae53c2f6b4ad4e540db2f20863be9d13
unknown <sebaslogo@gmail.com>
2022-11-18 10:58:31 -0600

    Starting webserver project

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ A grep clone made from following the tutorials in the Rust book
 ### Trie  
 A Trie data structure implemented in Rust. Learning the Box smart pointer in Rust.  
 
+### Hello
+[Multi-threaded webserver project from book](https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html)
+
+
 ### Huffman  
 Huffman encoding and decoding in Rust. Cargo layout n stuff.
---
+++ hello/src/main.rs
@@ -0,0 +1,32 @@
+use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::fs;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get= b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(), contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        // some other thing
+    }
+}
