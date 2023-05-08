114ad2a5ae1c0c7c43cd0ddf1bba8843eb2b7b32
pickles976 <sebaslogo@gmail.com>
2023-05-08 17:15:50 -0500

    Added basic integration to imu reader

--- imu-rs/src/main.rs
+++ imu-rs/src/main.rs
@@ -16,9 +16,11 @@ use esp32c3_hal::{
 };
 use esp_backtrace as _;
 use icm42670::{prelude::*, Address, Icm42670};
+use icm42670::{AccelOdr, GyroOdr, GyroRange};
 
 #[riscv_rt::entry]
 fn main() -> ! {
+
     let peripherals = Peripherals::take().unwrap();
     let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
@@ -48,6 +50,9 @@ fn main() -> ! {
     .unwrap();
 
     let mut icm = Icm42670::new(i2c, Address::Primary).unwrap();
+    icm.set_accel_odr(AccelOdr::Hz25);
+    icm.set_gyro_odr(GyroOdr::Hz25);
+    // icm.set_gyro_range(GyroRange::Deg250);
 
     loop {
         let accel_norm = icm.accel_norm().unwrap();
@@ -55,11 +60,11 @@ fn main() -> ! {
 
         writeln!(
             UsbSerialJtag,
-            "ACCEL  =  X: {:+.04} Y: {:+.04} Z: {:+.04}\t\tGYRO  =  X: {:+.04} Y: {:+.04} Z: {:+.04}",
+            "{{ \"ACCEL\": {{ \"X\": {:.04}, \"Y\": {:.04}, \"Z\": {:.04} }}, \"GYRO\": {{ \"X\": {:.04}, \"Y\": {:.04}, \"Z\": {:.04} }} }}",
             accel_norm.x, accel_norm.y, accel_norm.z, gyro_norm.x, gyro_norm.y, gyro_norm.z
         )
         .ok();
 
-        delay.delay_ms(100u32);
+        delay.delay_ms(10u32);
     }
 }
--- imu-rs/test_serial.py
+++ imu-rs/test_serial.py
@@ -1,8 +1,57 @@
+from cmath import sin
+from math import cos, pi
 import serial
-ser = serial.Serial('/dev/ttyACM0')  # open serial port
+import json
+import time
+from datetime import datetime
+
+### chip is rotated so X is Z axis
+### robot forward is Z, robot sideways is Y
+
+IMU_HZ = 1000.0
+dt = 1 / IMU_HZ
+# print(dt)
+start = time.time()
+
+def integrate_rotation(data, dt):
+    angular_z = data["GYRO"]["Y"]
+    return angular_z * dt
+
+def integrate_velocity(data, dt):
+    forward = data["ACCEL"]["Z"]
+    return forward * dt
+
+def integrate_position(angle, velocity, dt):
+    dvel = velocity * dt
+    rad = angle * pi / 180.0
+    return [cos(rad) * dvel, sin(rad) * dvel]
+    
+
+ser = serial.Serial('/dev/ttyACM0', baudrate=921600)  # open serial port
+ser.baudrate = 921600
 print(ser.name)         # check which port was really used
-# ser.write(b'hello')     # write a string
+
+angle = 0
+velocity = 0
+position = [0,0]
+
+start = time.time()
+
 while True:
-    s = ser.readlines(10)
-    print(s)
+
+    imu_string = ser.readline().decode('utf8')
+    imu_data = {}
+
+    try: 
+        imu_data = json.loads(imu_string)
+    except:
+        print("String not JSON format: %s"%imu_string)
+
+    if "GYRO" in imu_data:
+        dt = time.time() - start # time since last measurement
+        angle = angle + integrate_rotation(imu_data, dt)
+        velocity = velocity + integrate_velocity(imu_data, dt)
+        position = integrate_position(angle, velocity, dt)
+        start = time.time()
+        
 ser.close()             # close port
