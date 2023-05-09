ed80605a462f8aa7790f7277de51808d01bb9a96
pickles976 <sebaslogo@gmail.com>
2023-05-09 09:52:45 -0500

    added variables for axes

--- imu-rs/test_serial.py
+++ imu-rs/test_serial.py
@@ -7,6 +7,8 @@ from datetime import datetime
 
 ### chip is rotated so X is Z axis
 ### robot forward is Z, robot sideways is Y
+ROTATION_AXIS = "Y"
+FORWARD_AXIS = "Z"
 
 IMU_HZ = 1000.0
 dt = 1 / IMU_HZ
@@ -14,11 +16,11 @@ dt = 1 / IMU_HZ
 start = time.time()
 
 def integrate_rotation(data, dt):
-    angular_z = data["GYRO"]["Y"]
+    angular_z = data["GYRO"][ROTATION_AXIS]
     return angular_z * dt
 
 def integrate_velocity(data, dt):
-    forward = data["ACCEL"]["Z"]
+    forward = data["ACCEL"][FORWARD_AXIS]
     return forward * dt
 
 def integrate_position(angle, velocity, dt):
@@ -53,5 +55,6 @@ while True:
         velocity = velocity + integrate_velocity(imu_data, dt)
         position = integrate_position(angle, velocity, dt)
         start = time.time()
+        print(position)
         
 ser.close()             # close port
