06231b93ae10ae56ef41d3a5878a662fd4e8ff3d
pickles976 <sebaslogo@gmail.com>
2023-05-09 12:05:38 -0500

    Added code stuff

--- imu-rs/readme.md
+++ imu-rs/readme.md
@@ -1,2 +1,8 @@
 Use this to read from serial after flashing
 runner = "espflash --monitor"
+
+# Todo:
+1. Subscribe to controls input
+2. When controls input comes in, set previous measurement value to drift value
+3. Start measuring and integrating normally. Publish twist estimates at 5hz.
+4. When controls input stops, stop integrating
--- imu-rs/test_serial.py
+++ imu-rs/test_serial.py
@@ -14,21 +14,22 @@ FORWARD_AXIS = "X"
 
 IMU_HZ = 1000.0
 dt = 1 / IMU_HZ
-epsilon = 0.08
+epsilon = 0.005
 # print(dt)
 start = time.time()
 elapsed = 0
+
+coeff = 0
+bias = 0
 
 def integrate_rotation(data, dt):
     angular_z = data["G"][ROTATION_AXIS]
     return angular_z * dt
 
 def integrate_velocity(data, dt):
-    a = data["A"][FORWARD_AXIS] * 9.81 #g's to m/s^2
-    # forward = a if abs(a) > epsilon else 0
-    forward = a
-    print(forward * dt)
-    return forward * dt
+    acc = data["A"][FORWARD_AXIS] * 9.81 #g's to m/s^2
+    print(acc * dt)
+    return acc * dt
 
 def integrate_position(position, angle, velocity, dt):
     dvel = velocity * dt
@@ -41,9 +42,7 @@ ser = serial.Serial('/dev/ttyACM0', baudrate=921600)  # open serial port
 ser.baudrate = 921600
 print(ser.name)         # check which port was really used
 
-angle = 0
 velocity = 0
-position = [0,0]
 
 velocity_measurements = []
 time_measurements = []
@@ -66,6 +65,7 @@ while True:
         elapsed += dt
         a = imu_data["A"][FORWARD_AXIS] * 9.81 #g's to m/s^2
         velocity += a * dt
+        # velocity = a
         print(a)
         velocity_measurements.append(velocity)
         time_measurements.append(elapsed)
@@ -80,14 +80,18 @@ time_measurements = np.array(time_measurements, dtype=np.float32).reshape((-1, 1
 velocity_measurements = np.array(velocity_measurements, dtype=np.float32) # Y
 model = LinearRegression().fit(time_measurements, velocity_measurements)
 r_sq = model.score(time_measurements, velocity_measurements)
-b = model.intercept_
-a = model.coef_
+bias = model.intercept_
+coef = model.coef_[0]
 
 print(r_sq)
-print(b)
-print(a[0])
+print(bias)
+print(coef)
 
-exit()
+# exit()
+
+angle = 0
+velocity = 0
+position = [0,0]
 
 start = time.time()
 while True:
@@ -102,11 +106,13 @@ while True:
 
     if "G" in imu_data:
         dt = time.time() - start # time since last measurement
+        elapsed += dt
         angle = angle + integrate_rotation(imu_data, dt)
         velocity = velocity + integrate_velocity(imu_data, dt)
-        print(velocity)
-        # position = integrate_position(position, angle, velocity, dt)
-        # start = time.time()
+        corrected_vel = velocity - ((elapsed * coeff) + bias)
+        print(corrected_vel)
+        # position = integrate_position(position, angle, corrected_vel, dt)
+        start = time.time()
         # print(position)
         
 ser.close()             # close port
